2a741041c68ec4b1fcb974f164b90be7da8135f7
p4telj <pateljim@umich.edu>
2019-08-28 16:00:09 -0400

    [master] updated start script with new containers

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # if volume permission error: sudo chown 1000 jenkins/jenkins_home
 if [[ "$(uname -m)" =~ "x86" ]];
 then
-    docker-compose $1 up jenkins-x64
+    docker-compose $1 up jenkins-x64 mysql-x64 gitea-x64
 elif [[ "$(uname -m)" =~ "armv7" ]];
 then
-    sudo docker-compose $1 up jenkins-arm32
+    sudo docker-compose $1 up jenkins-arm32 mysql-arm32 gitea-arm32
 else
     echo "$(uname -m) is not currently supported. Add corresponding docker images"
 fi 
