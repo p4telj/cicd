96ae4ef30bf47185b1fd693375348687fabc847a
p4telj <pateljim@umich.edu>
2019-08-28 11:25:10 -0400

    [master] added option to include verbose

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 if [[ "$(uname -m)" =~ "x86" ]];
 then
-    docker-compose up jenkins-x64
+    docker-compose $1 up jenkins-x64
 elif [[ "$(uname -m)" =~ "armv7" ]];
 then
-    sudo docker-compose up jenkins-arm32
+    sudo docker-compose $1 up jenkins-arm32
 else
     echo "$(uname -m) is not currently supported. Add corresponding docker images"
 fi 
